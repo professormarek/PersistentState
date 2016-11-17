7b422b44df87131b79160afbec3dfc84aed4f416
marek.laskowski <marek.laskowski@senecacollege.ca>
2016-11-17 16:49:17 -0500

    completed the example using SQL

--- app/src/main/java/com/example/marek/persistentstate/MainActivity.java
+++ app/src/main/java/com/example/marek/persistentstate/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.marek.persistentstate;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
@@ -37,18 +39,49 @@ public class MainActivity extends AppCompatActivity {
         //the other is the database version
         public static final int DB_VERSION = 1;
 
+        private static final String SQL_CREATE_TABLE = "CREATE TABLE "+ MyDataEntry.TABLE_NAME +
+                 " (" + MyDataEntry._ID + " INTEGER PRIMARY KEY," + MyDataEntry.STUDENT_ID_COLUMN +
+                " TEXT," + MyDataEntry.GRADE_COLUMN + " TEXT )";
+        private static final String SQL_DELETE_QUERY = "DROP TABLE IF EXISTS " + MyDataEntry.TABLE_NAME;
+
+
         public MyDbHelper(Context context){
             super(context, DB_NAME, null, DB_VERSION);
         }
 
+        /**
+         * This framework method is called whenever the database is opened but doesn't exist yet
+         * @param db
+         */
         @Override
         public void onCreate(SQLiteDatabase db) {
-
+            System.out.println("Executing Query: SQL_CREATE_TABLE "  + SQL_CREATE_TABLE);
+            db.execSQL(SQL_CREATE_TABLE);
         }
 
+        /**
+         * this is another framework method that is called whenever the DB_VERSION is incremented
+         * @param db
+         * @param oldVersion
+         * @param newVersion
+         */
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            //shortcut: let's discard the table and start over
+            db.execSQL(SQL_DELETE_QUERY);
+            onCreate(db);
+        }
 
+        /**
+         * called whenever the database version is decremented
+         * @param db
+         * @param oldVersion
+         * @param newVersion
+         */
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            super.onDowngrade(db, oldVersion, newVersion);
+            onUpgrade(db, oldVersion,newVersion);
         }
     }
 
@@ -71,6 +104,8 @@ public class MainActivity extends AppCompatActivity {
 
         //read in any information from the preferences file if it exists
         loadPreferences();
+
+        loadDatabase();
     }
 
     @Override
@@ -124,7 +159,83 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
     }
 
+    /**
+     * demonstates how to access the database for writing!
+     */
     private void saveGrade(){
         //here we will write the student id and grade to the database
+
+        //ideally this work would be done in an AsyncTask - I leave this to you as an exercise
+
+        //let's get an instance of MyDbHelper - notice it needs a context
+        MyDbHelper dbHelper = new MyDbHelper(this);
+        //get a reference to the database in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        //create a new map of values, where column names are the keys
+        ContentValues newRow = new ContentValues();
+        //add the rows to the map
+        EditText studentIDBox = (EditText)findViewById(R.id.studentID);
+        String studentID = studentIDBox.getText().toString();
+        newRow.put(MyDataEntry.STUDENT_ID_COLUMN,studentID); //this adds the student ID to the record
+        EditText studentGradeBox = (EditText) findViewById(R.id.studentGrade);
+        String studentGrade = studentGradeBox.getText().toString();
+        newRow.put(MyDataEntry.GRADE_COLUMN,studentGrade);
+        System.out.println("Writing to database: " + studentID +", " +  studentGrade);
+        //Insert the new row, return the primary key value of the new row
+        //(in case we need it for some reason)
+        //the middle argument is what to insert if newRow is a null object (null)
+        long newRowId = db.insert(MyDataEntry.TABLE_NAME, null, newRow);
+        System.out.println("Result: " + newRowId);
+    }
+
+    /**
+     * this method shows how to access the database for reading
+     */
+    private void loadDatabase(){
+        //get a reference to MyDbHelper
+        MyDbHelper dbHelper = new MyDbHelper(this);
+        //get a readable database reference
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        //define which columns we want inlcluded in our query
+        String[] query_columns = {
+                MyDataEntry._ID,
+                MyDataEntry.STUDENT_ID_COLUMN,
+                MyDataEntry.GRADE_COLUMN
+        };
+        //create a select query
+        String selectQuery = MyDataEntry.STUDENT_ID_COLUMN + " = ?";
+        //these the arguments to my select query
+        String[] selectionArgs = {" Filter string "};
+
+        System.out.println("Executing select query!");
+
+        //define how we want the results to be ordered
+        String sortOrder = MyDataEntry.STUDENT_ID_COLUMN + " DESC";
+        //form our select query - get a cursor object.. see documentation
+        Cursor cursor = db.query(
+                MyDataEntry.TABLE_NAME, //which table to execute the query against
+                query_columns, //the columns that will be returned
+                null, //provide a WHERE clause - or null to return all rows
+                null, //arguments for the WHERE clause
+                null, //grouping
+                null, //filter
+                sortOrder //sorting order
+        );
+        //move to the first record result - returns false if there are no records
+        boolean hasMoreData = cursor.moveToFirst();
+        while(hasMoreData){
+            //get the value of each column...
+            long recordID = cursor.getLong(cursor.getColumnIndexOrThrow(MyDataEntry._ID));
+            String studentID = cursor.getString(cursor.getColumnIndexOrThrow(MyDataEntry.STUDENT_ID_COLUMN));
+            String studentGrade = cursor.getString(cursor.getColumnIndexOrThrow(MyDataEntry.GRADE_COLUMN));
+            //I'm going to write to the log, you will populate an ArrayList for the ListView
+            System.out.println("RECORD KEY: " + recordID + " Student ID: " + studentID + " Student Grade: " + studentGrade);
+            //advance to the next record - returns false if there are no more records
+            hasMoreData = cursor.moveToNext();
+        }
+
+
     }
 }
